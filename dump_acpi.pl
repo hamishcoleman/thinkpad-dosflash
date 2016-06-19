6bae228342acdb86f204a99be4bb15128c8a45e8
Hamish Coleman <hamish@zot.org>
2016-06-19 22:52:05 +1000

    Move towards a more complete ACPI table dumper, which required a better bios dump, so include a fmem fix

--- dump_acpi.pl
+++ dump_acpi.pl
@@ -6,6 +6,8 @@ use strict;
 #
 # Ref http://wiki.osdev.org/RSDP
 
+my $debug = 0;
+
 use IO::File;
 
 use Data::Dumper;
@@ -79,18 +81,27 @@ sub memr_read {
     my $region;
     # find the correct region
     for my $r (@{$db->{region}}) {
-        if ($phys_addr > $r->{phys_addr} && $phys_addr+$size < $r->{phys_addr}+$r->{size}) {
+        if ($phys_addr > $r->{phys_addr} && $phys_addr < $r->{phys_addr}+$r->{size}) {
             $region = $r;
             last;
         }
     }
 
     if (!defined($region)) {
-        die("unhandled address ",$phys_addr);
+        printf("unhandled address 0x%08x\n",$phys_addr);
+        return undef;
     }
 
     my $offset = $phys_addr - $region->{phys_addr};
     $offset += $region->{file_offset};
+
+    if ($debug) {
+        printf("0x%08x(%x) = 0x%08x (%s)\n",
+            $phys_addr,$size,
+            $offset,
+            $region->{filename},
+        );
+    }
 
     $region->{fh}->seek($offset,SEEK_SET);
     my $buf;
@@ -102,7 +113,7 @@ sub memr_read {
 sub find_rsdp {
     my $db = shift;
 
-    my $offset = 0xe0000;
+    my $offset = 0xf0000; # Note, this could be as early as 0xe0000
 
     while($offset < 0xfffff) {
         my $signature = memr_read($db,$offset,8);
@@ -119,13 +130,85 @@ sub find_rsdp {
 
 sub read_rsdp {
     my $db = shift;
+    my $addr = shift;
 
-    my $buf = memr_read($db,$db->{address}{rsdp},36);
+    my $buf = memr_read($db,$addr,36);
 
     my @fields = qw(signature checksum oemid revision rsdtaddress length xsdtaddress extendedchecksum reserved);
-    my @values = unpack("A[8]CA[6]cVVQCa[3]",$buf);
-    %{$db->{data}{rsdp}} =
-        map { $fields[$_] => $values[$_] } (0..scalar(@fields)-1);
+    my @values = unpack("A8CA6cVVQCa3",$buf);
+    my %rsdp = map { $fields[$_] => $values[$_] } (0..scalar(@fields)-1);
+
+    $rsdp{_addr} = $addr;
+    return \%rsdp;
+}
+
+sub dump_rsdp {
+    my $rsdp = shift;
+
+    printf("0x%08x: %s(%i) %s rsdt=0x%08x xsdt=0x%08x\n",
+        $rsdp->{_addr},
+        $rsdp->{signature}, $rsdp->{revision},
+        $rsdp->{oemid},
+        $rsdp->{rsdtaddress}, $rsdp->{xsdtaddress},
+    );
+}
+
+sub read_SDT {
+    my $db = shift;
+    my $addr = shift;
+
+    my $header_len = 36;
+    my $buf = memr_read($db,$addr,$header_len);
+
+    my @fields = qw(signature length revision checksum oemid oemtableid oemrevision creatorid creatorrevision);
+    my @values = unpack("A4VccA6A8VA4V",$buf);
+    my %header = map { $fields[$_] => $values[$_] } (0..scalar(@fields)-1);
+
+    return undef if ($header{signature} eq "\xff\xff\xff\xff");
+
+    my $sdt;
+    $sdt->{_header} = \%header;
+    $sdt->{_addr} = $addr;
+
+    my $tablename = $header{signature};
+    my $i = 0;
+    while (defined($db->{SDT}{$tablename})) {
+        $tablename = sprintf("%s:%i",$header{signature},++$i);
+    }
+    $db->{SDT}{$tablename} = $sdt;
+
+    my $remainder = $header{length} - $header_len;
+    return $sdt if ($remainder<=0);
+
+    $sdt->{_data} = memr_read($db,$addr+$header_len,$remainder);
+    return $sdt;
+}
+
+sub dump_SDT {
+    my $sdt = shift;
+    my $header = $sdt->{_header};
+
+    printf("0x%08x: %s(%i) %s %s(%i) %s(%i)\n",
+        $sdt->{_addr},
+        $header->{signature}, $header->{revision},
+        $header->{oemid},
+        $header->{oemtableid}, $header->{oemrevision},
+        $header->{creatorid}, $header->{creatorrevision},
+    );
+}
+
+sub read_XSDT {
+    my $db = shift;
+    my $addr = shift;
+
+    my $SDT = read_SDT($db,$addr);
+
+    my @tables = unpack("Q*",$SDT->{_data});
+
+    delete $SDT->{_data};
+    $SDT->{tables} = \@tables;
+
+    return $SDT;
 }
 
 sub main() {
@@ -137,11 +220,20 @@ sub main() {
     my $db = {};
     load_configfile($db,$configfile);
 
-    $db->{address}{rsdp} = find_rsdp($db);
-    if (!$db->{address}{rsdp}) {
+    $db->{address}{RSDP} = find_rsdp($db);
+    if (!$db->{address}{RSDP}) {
         die("Could not find RSDP\n");
     }
-    read_rsdp($db);
+
+    $db->{RSDP} = read_rsdp($db,$db->{address}{RSDP});
+    read_XSDT($db,$db->{RSDP}{xsdtaddress});
+
+    dump_rsdp($db->{RSDP});
+    dump_SDT($db->{SDT}{XSDT});
+
+    for my $addr (@{$db->{SDT}{XSDT}{tables}}) {
+        dump_SDT(read_SDT($db,$addr));
+    }
 
     print Dumper($db);
 
